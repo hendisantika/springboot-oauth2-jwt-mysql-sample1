77762601e1ad426d052b7b93f3b50ecb57c03225
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-17 06:25:08 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/SpringbootOauth2JwtMysqlSample1Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootoauth2jwtmysqlsample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOauth2JwtMysqlSample1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOauth2JwtMysqlSample1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootoauth2jwtmysqlsample1/SpringbootOauth2JwtMysqlSample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootoauth2jwtmysqlsample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOauth2JwtMysqlSample1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
