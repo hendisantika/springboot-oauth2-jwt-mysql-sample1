43645a2830556f2bb442b2192b6adc3ff3a44f08
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-17 06:46:58 +0700

    #19 - Add handleMissingServletRequestParameter in RestExceptionHandler

--- src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/error/RestExceptionHandler.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/error/RestExceptionHandler.java
@@ -3,7 +3,12 @@ package com.hendisantika.springbootoauth2jwtmysqlsample1.error;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
@@ -19,5 +24,21 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 @Slf4j
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    /**
+     * Handle MissingServletRequestParameterException. Triggered when a 'required' request parameter is missing.
+     *
+     * @param ex      MissingServletRequestParameterException
+     * @param headers HttpHeaders
+     * @param status  HttpStatus
+     * @param request WebRequest
+     * @return the ApiError object
+     */
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(
+            MissingServletRequestParameterException ex, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+        String error = ex.getParameterName() + " parameter is missing";
+        return buildResponseEntity(new ApiError(BAD_REQUEST, error, ex));
+    }
 
 }
