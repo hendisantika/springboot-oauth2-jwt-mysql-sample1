36c57c9fa47a4fb20a93eff772c3ee7558ab238d
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-17 06:52:49 +0700

    #32 - Add more methods in ApiError

--- src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/error/ApiError.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/error/ApiError.java
@@ -72,4 +72,41 @@ class ApiError {
             fieldError.getRejectedValue(),
             fieldError.getDefaultMessage());
   }
+
+  void addValidationErrors(List<FieldError> fieldErrors) {
+    fieldErrors.forEach(this::addValidationError);
+  }
+
+  private void addValidationError(ObjectError objectError) {
+    this.addValidationError(
+            objectError.getObjectName(),
+            objectError.getDefaultMessage());
+  }
+
+  void addValidationError(List<ObjectError> globalErrors) {
+    globalErrors.forEach(this::addValidationError);
+  }
+
+  /**
+   * Utility method for adding error of ConstraintViolation. Usually when a @Validated validation fails.
+   *
+   * @param cv the ConstraintViolation
+   */
+  private void addValidationError(ConstraintViolation<?> cv) {
+    this.addValidationError(
+            cv.getRootBeanClass().getSimpleName(),
+            ((PathImpl) cv.getPropertyPath()).getLeafNode().asString(),
+            cv.getInvalidValue(),
+            cv.getMessage());
+  }
+
+  void addValidationErrors(Set<ConstraintViolation<?>> constraintViolations) {
+    constraintViolations.forEach(this::addValidationError);
+  }
+
+
+  abstract class ApiSubError {
+
+  }
+
 }
