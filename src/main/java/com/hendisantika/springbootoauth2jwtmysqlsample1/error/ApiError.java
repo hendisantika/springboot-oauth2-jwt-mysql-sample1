67f31c3e27a60e566b7866c94881a632401806ce
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-17 06:52:12 +0700

    #30 - Add more constructor in ApiError

--- src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/error/ApiError.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/error/ApiError.java
@@ -25,6 +25,26 @@ class ApiError {
   private String debugMessage;
   private List<ApiSubError> subErrors;
 
-  public ApiError(HttpStatus badRequest) {
+  private ApiError() {
+    timestamp = LocalDateTime.now();
+  }
+
+  ApiError(HttpStatus status) {
+    this();
+    this.status = status;
+  }
+
+  ApiError(HttpStatus status, Throwable ex) {
+    this();
+    this.status = status;
+    this.message = "Unexpected error";
+    this.debugMessage = ex.getLocalizedMessage();
+  }
+
+  ApiError(HttpStatus status, String message, Throwable ex) {
+    this();
+    this.status = status;
+    this.message = message;
+    this.debugMessage = ex.getLocalizedMessage();
   }
 }
