23248bcc660dcb70ebbaf83109391b73dfe88d5f
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-17 06:45:30 +0700

    #17 - Add initUsers in LoadDatabase

--- src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/repository/LoadDatabase.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/repository/LoadDatabase.java
@@ -1,8 +1,11 @@
 package com.hendisantika.springbootoauth2jwtmysqlsample1.repository;
 
+import com.hendisantika.springbootoauth2jwtmysqlsample1.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -40,4 +43,21 @@ public class LoadDatabase {
     @Value("clientSecret:secret")
     private String clientSecret;
 
+    int rnd(int size) {
+        return (int) (Math.random() * size);
+    }
+
+    @Bean
+    CommandLineRunner initUsers(UserRepository repo) {
+
+        return args -> {
+            for (int i = 0; i < users.length; i++) {
+                String email = users[i] + "@" + users[i] + ".com";
+                User.Role role = i > 1 ? User.Role.USER : i == 0 ? User.Role.ADMIN : User.Role.USER_MANAGER;
+                double minGleePerDay = rnd(1000 * feelings.length);
+                String pwd = passwordEncoder.encode("pwd");
+                log.info("save {}", repo.save(new User(null, email, pwd, role, minGleePerDay, null)));
+            }
+        };
+    }
 }
