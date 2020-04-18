13008ba240b8643dc4876910953420075cc910cb
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 16:15:17 +0700

    #56 - Add all API in UserController

--- src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.hendisantika.springbootoauth2jwtmysqlsample1.controller;
 
+import com.hendisantika.springbootoauth2jwtmysqlsample1.model.User;
 import com.hendisantika.springbootoauth2jwtmysqlsample1.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +35,15 @@ class UserController {
     UserController(UserRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping
+    Page<User> all(@PageableDefault(size = Integer.MAX_VALUE) Pageable pageable, OAuth2Authentication authentication) {
+        String auth = (String) authentication.getUserAuthentication().getPrincipal();
+        String role = authentication.getAuthorities().iterator().next().getAuthority();
+        if (role.equals(User.Role.USER.name())) {
+            return repository.findAllByEmail(auth, pageable);
+        }
+        return repository.findAll(pageable);
     }
 }
