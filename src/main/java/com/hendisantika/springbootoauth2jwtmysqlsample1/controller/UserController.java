85c7865f7de05aedf36852c42a10de6b0ef290b8
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 16:17:59 +0700

    #60 - Add update API in UserController

--- src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/UserController.java
@@ -14,9 +14,13 @@ import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -72,6 +76,17 @@ class UserController {
     @PostAuthorize("!hasAuthority('USER') || (returnObject != null && returnObject.email == authentication.principal)")
     User one(@PathVariable Long id) {
         return repository.findById(id).orElseThrow(() -> new EntityNotFoundException(User.class, "id", id.toString()));
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("!hasAuthority('USER') || (authentication.principal == @userRepository.findById(#id).orElse(new net" +
+            ".reliqs.gleeometer.users.User()).email)")
+    void update(@PathVariable Long id, @Valid @RequestBody User res) {
+        User u = repository.findById(id).orElseThrow(() -> new EntityNotFoundException(User.class, "id",
+                id.toString()));
+        res.setPassword(u.getPassword());
+        res.setMessages(u.getMessages());
+        repository.save(res);
     }
 
 }
