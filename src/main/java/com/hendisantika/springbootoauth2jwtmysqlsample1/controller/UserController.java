7e38d61c5ca6d175e8448e0b1cee0b8a2c17ef3a
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 16:16:05 +0700

    #58 - Add findByEmail API in UserController

--- src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.hendisantika.springbootoauth2jwtmysqlsample1.controller;
 
+import com.hendisantika.springbootoauth2jwtmysqlsample1.error.EntityNotFoundException;
 import com.hendisantika.springbootoauth2jwtmysqlsample1.model.User;
 import com.hendisantika.springbootoauth2jwtmysqlsample1.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.validation.annotation.Validated;
@@ -56,6 +58,12 @@ class UserController {
             return repository.findAllByEmailContainsAndEmail(email, auth, pageable);
         }
         return repository.findByEmailContains(email, pageable);
+    }
+
+    @GetMapping("/findByEmail")
+    @PreAuthorize("!hasAuthority('USER') || (authentication.principal == #email)")
+    User findByEmail(@RequestParam String email, OAuth2Authentication authentication) {
+        return repository.findByEmail(email).orElseThrow(() -> new EntityNotFoundException(User.class, "email", email));
     }
 
 }
