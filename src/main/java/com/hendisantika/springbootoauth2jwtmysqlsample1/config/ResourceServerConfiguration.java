34b4a3ddd23934335b0eb7a24c45280e9aeab058
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 16:08:58 +0700

    #46 - Add ResourceServerConfiguration

---
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/config/ResourceServerConfiguration.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springbootoauth2jwtmysqlsample1.config;
+
+import com.hendisantika.springbootoauth2jwtmysqlsample1.error.CustomAuthenticationEntryPoint;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-oauth2-jwt-mysql-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/04/20
+ * Time: 16.08
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    public ResourceServerConfiguration(CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
+        this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
+    }
+
+}
