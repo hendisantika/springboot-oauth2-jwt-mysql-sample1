587a0a299ec7b77b7711426ae4aaaa32c48b7a22
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 16:13:26 +0700

    #52 - Add SignInController

---
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/SignInController.java
@@ -0,0 +1,33 @@
+package com.hendisantika.springbootoauth2jwtmysqlsample1.controller;
+
+import com.hendisantika.springbootoauth2jwtmysqlsample1.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-oauth2-jwt-mysql-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/04/20
+ * Time: 16.12
+ */
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("/api/signin")
+public class SignInController {
+
+    private final UserRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public SignInController(UserRepository repository, PasswordEncoder passwordEncoder) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
+}
