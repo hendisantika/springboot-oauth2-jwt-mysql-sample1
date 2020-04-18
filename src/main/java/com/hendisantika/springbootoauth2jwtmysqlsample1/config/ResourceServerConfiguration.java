e78286ee060cc0f2f09c39c5960fe91e132d8abd
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 16:09:17 +0700

    #47 - Add configure in ResourceServerConfiguration

--- src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/config/ResourceServerConfiguration.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/config/ResourceServerConfiguration.java
@@ -1,9 +1,13 @@
 package com.hendisantika.springbootoauth2jwtmysqlsample1.config;
 
+import com.hendisantika.springbootoauth2jwtmysqlsample1.error.CustomAccessDeniedHandler;
 import com.hendisantika.springbootoauth2jwtmysqlsample1.error.CustomAuthenticationEntryPoint;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +27,25 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
         this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
     }
 
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId("api");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .antMatcher("/api/**")
+                .authorizeRequests()
+                .antMatchers("/api/signin**").permitAll()
+                .antMatchers("/api/signin/**").permitAll()
+                .antMatchers("/api/glee**").hasAnyAuthority("ADMIN", "USER")
+                .antMatchers("/api/users**").hasAuthority("ADMIN")
+                .antMatchers("/api/**").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(customAuthenticationEntryPoint).accessDeniedHandler(new CustomAccessDeniedHandler());
+    }
 }
