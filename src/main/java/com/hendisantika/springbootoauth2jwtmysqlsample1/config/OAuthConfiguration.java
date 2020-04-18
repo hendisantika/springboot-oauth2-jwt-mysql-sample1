05ca8d2b5043b75a0e81ba527f8fd2b0822f255e
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 16:10:15 +0700

    #48 - Add OAuthConfiguration

---
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/config/OAuthConfiguration.java
@@ -0,0 +1,52 @@
+package com.hendisantika.springbootoauth2jwtmysqlsample1.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-oauth2-jwt-mysql-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/04/20
+ * Time: 16.09
+ */
+@Configuration
+@EnableAuthorizationServer
+public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    private final AuthenticationManager authenticationManager;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final UserDetailsService userService;
+
+    @Value("${jwt.clientId:glee-o-meter}")
+    private String clientId;
+
+    @Value("${jwt.client-secret:secret}")
+    private String clientSecret;
+
+    @Value("${jwt.accessTokenValidititySeconds:43200}") // 12 hours
+    private int accessTokenValiditySeconds;
+
+    @Value("${jwt.authorizedGrantTypes:password,authorization_code,refresh_token}")
+    private String[] authorizedGrantTypes;
+
+    @Value("${jwt.refreshTokenValiditySeconds:2592000}") // 30 days
+    private int refreshTokenValiditySeconds;
+
+    public OAuthConfiguration(AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder,
+                              UserDetailsService userService) {
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
+    }
+}
+
