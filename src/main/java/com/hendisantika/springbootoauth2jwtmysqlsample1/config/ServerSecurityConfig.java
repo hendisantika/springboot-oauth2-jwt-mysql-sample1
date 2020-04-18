ba552528402892bf16de3aa1912edba66cfd393e
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 16:08:09 +0700

    #45 - Add passwordEncoder & authenticationManagerBean in ServerSecurityConfig

--- src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/config/ServerSecurityConfig.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/config/ServerSecurityConfig.java
@@ -4,11 +4,14 @@ import com.hendisantika.springbootoauth2jwtmysqlsample1.error.CustomAuthenticati
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -39,6 +42,17 @@ public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setPasswordEncoder(passwordEncoder());
         provider.setUserDetailsService(userDetailsService);
         return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
