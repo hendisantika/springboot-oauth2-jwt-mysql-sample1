ad93f24a4eb4d449ad18aeea2947d57275f762c4
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 16:11:13 +0700

    #49 - Add configure in OAuthConfiguration

--- src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/config/OAuthConfiguration.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/config/OAuthConfiguration.java
@@ -1,12 +1,16 @@
 package com.hendisantika.springbootoauth2jwtmysqlsample1.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -47,6 +51,32 @@ public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
         this.authenticationManager = authenticationManager;
         this.passwordEncoder = passwordEncoder;
         this.userService = userService;
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient(clientId)
+                .secret(passwordEncoder.encode(clientSecret))
+                .accessTokenValiditySeconds(accessTokenValiditySeconds)
+                .refreshTokenValiditySeconds(refreshTokenValiditySeconds)
+                .authorizedGrantTypes(authorizedGrantTypes)
+                .scopes("read", "write")
+                .resourceIds("api");
+    }
+
+    @Override
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints
+                .accessTokenConverter(accessTokenConverter())
+                .userDetailsService(userService)
+                .authenticationManager(authenticationManager);
+    }
+
+    @Bean
+    JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        return converter;
     }
 }
 
