3c5e25758c6b621685174e2740eb74716464f398
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 16:13:54 +0700

    #53 - Add signin API in SignInController

--- src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/SignInController.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/SignInController.java
@@ -1,10 +1,13 @@
 package com.hendisantika.springbootoauth2jwtmysqlsample1.controller;
 
+import com.hendisantika.springbootoauth2jwtmysqlsample1.model.User;
 import com.hendisantika.springbootoauth2jwtmysqlsample1.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,5 +32,11 @@ public class SignInController {
     public SignInController(UserRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping
+    User signin(@RequestParam String email, @RequestParam String password) {
+        User u = new User(null, email, passwordEncoder.encode(password), User.Role.USER, 0D, null);
+        return repository.save(u);
     }
 }
