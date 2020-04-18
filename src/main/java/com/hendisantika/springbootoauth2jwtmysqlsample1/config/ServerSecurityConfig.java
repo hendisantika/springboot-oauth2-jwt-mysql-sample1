4a0e73778010fe07122cd01b8716fbbded6b7adc
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 16:07:06 +0700

    #43 - Add ServerSecurityConfig

---
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/config/ServerSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.hendisantika.springbootoauth2jwtmysqlsample1.config;
+
+import com.hendisantika.springbootoauth2jwtmysqlsample1.error.CustomAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-oauth2-jwt-mysql-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/04/20
+ * Time: 16.06
+ */
+@Configuration
+//@Order(1)
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
+public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    private final UserDetailsService userDetailsService;
+
+    public ServerSecurityConfig(CustomAuthenticationEntryPoint customAuthenticationEntryPoint, @Qualifier("userService")
+            UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+}
