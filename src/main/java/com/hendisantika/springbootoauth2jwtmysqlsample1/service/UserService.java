5b6a9decd00f1e487450d68954a017c4df172faf
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 16:12:32 +0700

    #51 - Add UserService

---
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/service/UserService.java
@@ -0,0 +1,40 @@
+package com.hendisantika.springbootoauth2jwtmysqlsample1.service;
+
+import com.hendisantika.springbootoauth2jwtmysqlsample1.model.User;
+import com.hendisantika.springbootoauth2jwtmysqlsample1.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-oauth2-jwt-mysql-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/04/20
+ * Time: 16.12
+ */
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user =
+                repository.findByEmail(username).orElseThrow(() -> new RuntimeException("User not found: " + username));
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().name());
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                Arrays.asList(authority));
+    }
+}
