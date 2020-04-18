37e3b681e6f4f2e525478aff1590546c0cd55a82
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 16:16:27 +0700

    #59 - Add one API in UserController

--- src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/UserController.java
@@ -7,11 +7,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,6 +66,12 @@ class UserController {
     @PreAuthorize("!hasAuthority('USER') || (authentication.principal == #email)")
     User findByEmail(@RequestParam String email, OAuth2Authentication authentication) {
         return repository.findByEmail(email).orElseThrow(() -> new EntityNotFoundException(User.class, "email", email));
+    }
+
+    @GetMapping("/{id}")
+    @PostAuthorize("!hasAuthority('USER') || (returnObject != null && returnObject.email == authentication.principal)")
+    User one(@PathVariable Long id) {
+        return repository.findById(id).orElseThrow(() -> new EntityNotFoundException(User.class, "id", id.toString()));
     }
 
 }
