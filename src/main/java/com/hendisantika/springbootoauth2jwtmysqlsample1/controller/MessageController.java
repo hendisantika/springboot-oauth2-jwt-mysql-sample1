2475f2d4ce9a93e572496caab148e3b2761aeede
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-19 14:04:58 +0700

    #65 - Add searchMessages API in MessageController

--- src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/MessageController.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/MessageController.java
@@ -7,11 +7,16 @@ import com.hendisantika.springbootoauth2jwtmysqlsample1.repository.MessageReposi
 import com.hendisantika.springbootoauth2jwtmysqlsample1.repository.UserRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 /**
  * Created by IntelliJ IDEA.
@@ -46,4 +51,30 @@ public class MessageController {
         }
         return messageRepository.findAll(pageable);
     }
+
+    @GetMapping("/search")
+    Page<Message> searchMessages(
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            @RequestParam(value = "fromDate", required = false) LocalDate fromDate,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            @RequestParam(value = "toDate", required = false) LocalDate toDate,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+            @RequestParam(value = "fromTime", required = false) LocalTime fromTime,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+            @RequestParam(value = "toTime", required = false) LocalTime toTime,
+            @RequestParam(value = "text", required = false) String text,
+            @RequestParam(value = "value", required = false) Double cal,
+            @RequestParam(value = "userId", required = false) Long userId,
+            Pageable pageable, OAuth2Authentication authentication) {
+        String auth = (String) authentication.getUserAuthentication().getPrincipal();
+        String role = authentication.getAuthorities().iterator().next().getAuthority();
+        if (role.equals(User.Role.USER.name())) {
+            User user = userRepository.findByEmail(auth).orElseThrow(() -> new EntityNotFoundException(User.class,
+                    "email", auth));
+            userId = user.getId();
+            return messageRepository.filter(fromDate, toDate, fromTime, toTime, text, cal, userId, pageable);
+        }
+        return messageRepository.filter(fromDate, toDate, fromTime, toTime, text, cal, userId, pageable);
+    }
+
 }
