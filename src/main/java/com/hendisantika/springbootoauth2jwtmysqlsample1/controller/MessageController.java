b36aaa4fbb63ab9879f125664f3832bc1d89f848
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-19 14:03:16 +0700

    #64 - Add allMessages API in MessageController

--- src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/MessageController.java
+++ src/main/java/com/hendisantika/springbootoauth2jwtmysqlsample1/controller/MessageController.java
@@ -1,8 +1,15 @@
 package com.hendisantika.springbootoauth2jwtmysqlsample1.controller;
 
+import com.hendisantika.springbootoauth2jwtmysqlsample1.error.EntityNotFoundException;
+import com.hendisantika.springbootoauth2jwtmysqlsample1.model.Message;
+import com.hendisantika.springbootoauth2jwtmysqlsample1.model.User;
 import com.hendisantika.springbootoauth2jwtmysqlsample1.repository.MessageRepository;
 import com.hendisantika.springbootoauth2jwtmysqlsample1.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +33,17 @@ public class MessageController {
     MessageController(MessageRepository repository, UserRepository userRepository) {
         this.messageRepository = repository;
         this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    Page<Message> allMessages(Pageable pageable, OAuth2Authentication authentication) {
+        String auth = (String) authentication.getUserAuthentication().getPrincipal();
+        String role = authentication.getAuthorities().iterator().next().getAuthority();
+        if (role.equals(User.Role.USER.name())) {
+            User user = userRepository.findByEmail(auth).orElseThrow(() -> new EntityNotFoundException(User.class,
+                    "email", auth));
+            return messageRepository.findAllByUser(user, pageable);
+        }
+        return messageRepository.findAll(pageable);
     }
 }
